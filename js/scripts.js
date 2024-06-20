0886550c0127388471ca8f66f9928bbf67db9b47
Patrick Oliveira <joaopatrick65@gmail.com>
2024-06-19 22:34:26 -0300

    adicionado javascript
    adicionado javascript e um mock para testar funcionalidade do botao

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById('btn-div');
+var container = document.querySelector('.filme');
+btn.addEventListener('click', function() {
+    
+  if(container.style.display === 'block') {
+      container.style.display = 'none';
+  } else {
+      container.style.display = 'block';
+  }
+});
