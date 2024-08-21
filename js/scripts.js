f15050305d89420f83966a8592515fc5e6127875
Patrick Oliveira <joaopatrick65@gmail.com>
2024-08-20 21:52:44 -0300

    botoes de generos funcionais

--- js/scripts.js
+++ js/scripts.js
@@ -3,84 +3,64 @@ const baseURL = 'https://api.themoviedb.org/3';
 
 async function getRandomMovie() {
     try {
-        const response = await fetch(`${baseURL}/discover/movie?api_key=${apiKey}&language=pt-BR`);
-        const data = await response.json();
-        const movies = data.results;
-        const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-        displayMovie(randomMovie);
+        const response = await fetch(`${baseURL}/discover/movie?api_key=${apiKey}&language=pt-BR`); // chamada para API
+        const data = await response.json(); // conversão para JSON
+        const movies = data.results; // armazena os resultados na variável
+        const randomMovie = movies[Math.floor(Math.random() * movies.length)]; // pega um filme aleatório da lista
+        displayMovie(randomMovie); //chama a função DisplayMovie e usa randomMovie como argumento
     } catch (error) {
         console.error('Erro ao obter filme:', error);
     }
 }
 
-async function topRated() {
+async function getRandomType(genreId) {
     try {
-    const response = await fetch(`${baseURL}/movie/top_rated?api_key=${apiKey}&language=pt-BR`);
-    const data = await response.json();
-    const movies = data.results;
-    const top10Movies = movies.slice(0, 10);
-    displayTop10Movies(top10Movies)
+        const response = await fetch(`${baseURL}/discover/movie?api_key=${apiKey}&language=pt-BR&with_genres=${genreId}`); // chamada para API
+        const data = await response.json(); // conversão para JSON
+        const movies = data.results; // armazena os resultados na variável
+        const randomMovie = movies[Math.floor(Math.random() * movies.length)]; // pega um filme aleatório da lista
+        displayMovie(randomMovie); //chama a função DisplayMovie e usa randomMovie como argumento
     } catch (error) {
         console.error('Erro ao obter filme:', error);
-    }
+    }    
 }
 
 function displayMovie(movie) {
-    const movieContainer = document.getElementById('movie-container');
+    const movieContainer = document.getElementById('movie-container'); // seleciona o conteiner onde aparecerá as informações do filme
     movieContainer.innerHTML = `
         <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
         <p class="titulo-filme">${movie.title}</p>
         <p>${movie.overview}</p>
         <p class="rating">Nota: ${movie.vote_average.toFixed(1)}/10</p>
-    `;
-    movieContainer.style.display = 'block';
+    `; // insere as informações dentro do local especificado
+    movieContainer.style.display = 'block'; // deixa o conteiner visível
 }
-
-function displayTop10Movies(movies) {
-    const movieContainer = document.getElementById('carrossel');
-    movieContainer.innerHTML = '';
-
-    movies.forEach(movie => {
-        // Cria uma div para cada filme
-        const movieDiv = document.createElement('div');
-        movieDiv.classList.add('movie-item');
-
-        // Cria um elemento para o título
-        const movieTitle = document.createElement('p');
-        movieTitle.textContent = movie.title;
-        movieDiv.appendChild(movieTitle);
-
-        // Cria uma imagem para o pôster
-        const moviePoster = document.createElement('img');
-        moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        moviePoster.alt = movie.title;
-        movieDiv.appendChild(moviePoster);
-
-        // Adiciona a div do filme ao container
-        movieContainer.appendChild(movieDiv);
-    });
-}
-
-const carouselList = document.getElementById('carrossel');
-const prevArrow = document.querySelector('.prev-arrow');
-const nextArrow = document.querySelector('.next-arrow');
-let currentPosition = 0;
-const step = 100; // Largura de cada item do carrossel
-
-nextArrow.addEventListener('click', () => {
-    if (currentPosition > -step * (carouselList.children.length - 1)) {
-        currentPosition -= step;
-        carouselList.style.transform = `translateX(${currentPosition}%)`;
-    }
-});
-
-prevArrow.addEventListener('click', () => {
-    if (currentPosition < 0) {
-        currentPosition += step;
-        carouselList.style.transform = `translateX(${currentPosition}%)`;
-    }
-});
-
 
 document.getElementById('btn-div').addEventListener('click', getRandomMovie);
-document.addEventListener('load', topRated())
+document.getElementById('q1').addEventListener('click', function() {
+    getRandomType(28);
+});
+document.getElementById('q2').addEventListener('click', function() {
+    getRandomType(18);
+});
+document.getElementById('q3').addEventListener('click', function() {
+    getRandomType(10749);
+});
+document.getElementById('q4').addEventListener('click', function() {
+    getRandomType(35);
+});
+document.getElementById('q5').addEventListener('click', function() {
+    getRandomType(12);
+});
+document.getElementById('q6').addEventListener('click', function() {
+    getRandomType(878);
+});
+document.getElementById('q7').addEventListener('click', function() {
+    getRandomType(27);
+});
+document.getElementById('q8').addEventListener('click', function() {
+    getRandomType(53);
+});
+document.getElementById('q9').addEventListener('click', function() {
+    getRandomType(16);
+});
